05d5aca2571b25d6c1717aa25288ffd81c2d4a9a
Vivek kv <Vivek kv>
2016-04-07 08:21:55 +0530

    update date 7/4/2016

---
+++ farmers_market/assets/js/new_counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  incrementVar = 0;
+  $('.inc.button').click(function(){
+      var $this = $(this),
+          $input = $this.prev('input'),
+          $parent = $input.closest('div'),
+          newValue = parseInt($input.val())+1;
+      $parent.find('.inc').addClass('a'+newValue);
+      $input.val(newValue);
+      incrementVar += newValue;
+  });
+  $('.dec.button').click(function(){
+      var $this = $(this),
+          $input = $this.next('input'),
+          $parent = $input.closest('div'),
+          newValue = parseInt($input.val())-1;
+          if(newValue>=0){
+              $parent.find('.inc').addClass('a'+newValue);
+              $input.val(newValue);
+              incrementVar += newValue;
+            }
+  });
+});
